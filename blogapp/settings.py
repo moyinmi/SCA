120ca24a01c1fc0401b635785586cfdf6c3ede49
moyinmi <agbamoyin@gmail.com>
2020-09-04 13:45:25 +0100

    fixed bugs

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -114,6 +114,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL ='/media/'
